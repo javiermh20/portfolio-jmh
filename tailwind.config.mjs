27b634825406191fa96055c39d8267baa9595f71
javiermh20 <javiermejiah20@gmail.com>
2024-06-30 17:18:04 -0600

    Agregado de Iconos para el carrusel y Header Actualizado
    1. Agregado de iconos de las tecnologias que se utilizar
    2. Cambio en el tailwind.js para el manejo de las animaciones
    3. Agregado de hover en el header
    4. Nuevo componente llamado caurosel para el carrusel de tecnologias
    
    Listo para despliegue en host

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,17 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
-	},
+		extend: {
+		  animation: {
+			'infinite-scroll': 'infinite-scroll 25s linear infinite',
+		  },
+		  keyframes: {
+			'infinite-scroll': {
+			  from: { transform: 'translateX(0)' },
+			  to: { transform: 'translateX(-100%)' },
+			}
+		  }                    
+		},
+	  },
 	plugins: [],
 }
